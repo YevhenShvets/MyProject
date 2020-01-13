cd663cda92b5b9b4061e1b5a241361fce0c0e98b
Yevhen_06 <46687034+YevhenShvets@users.noreply.github.com>
2020-01-13 15:40:14 +0200

    1

--- app/src/main/java/com/yevhen/project/SplashScreenActivity.java
+++ app/src/main/java/com/yevhen/project/SplashScreenActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -12,6 +16,42 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        final ImageView img1 = (ImageView) findViewById(R.id.splash_logo1);
+        final ImageView img2 = (ImageView) findViewById(R.id.splash_logo2);
+        final ImageView img3 = (ImageView) findViewById(R.id.splash_logo3);
+        final ImageView img4 = (ImageView) findViewById(R.id.splash_logo4);
+        img2.setVisibility(View.INVISIBLE);
+        img3.setVisibility(View.INVISIBLE);
+        img4.setVisibility(View.INVISIBLE);
+
+        Handler handler1 = new Handler();
+        handler1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                img1.setVisibility(View.INVISIBLE);
+                img3.setVisibility(View.VISIBLE);
+            }
+        },1200);
+
+        Handler handler2 = new Handler();
+        handler2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                img3.setVisibility(View.INVISIBLE);
+                img4.setVisibility(View.VISIBLE);
+            }
+        },2400);
+
+        Handler handler3 = new Handler();
+        handler1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                img4.setVisibility(View.INVISIBLE);
+                img2.setVisibility(View.VISIBLE);
+            }
+        },3600);
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
@@ -20,6 +60,6 @@ public class SplashScreenActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },6000);
+        },5000);
     }
 }
